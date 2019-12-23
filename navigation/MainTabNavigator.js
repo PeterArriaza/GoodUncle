02c96d4b2e646890e4dd0080ea93f4791cf1a2e0
PeterArriaza <peter.arriaza@gmail.com>
2019-12-23 13:16:10 -0500

    added text and styling to landing page

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { Platform } from "react-native";
+import { Platform, TouchableOpacity, Text } from "react-native";
 import { createStackNavigator } from "react-navigation-stack";
 import { createBottomTabNavigator } from "react-navigation-tabs";
-
+import Icon from "@expo/vector-icons/Ionicons";
 import TabBarIcon from "../components/TabBarIcon";
 import MenuScreen from "../screens/MenuScreen";
 import CartScreen from "../screens/CartScreen";
@@ -16,7 +16,27 @@ const config = Platform.select({
 const MenuStack = createStackNavigator(
   {
     Menu: MenuScreen,
-    MenuItemDetail: MenuItemDetailScreen
+    MenuItemDetail: {
+      screen: MenuItemDetailScreen,
+      navigationOptions: {
+        headerRight: (
+          <TouchableOpacity
+            style={{
+              marginRight: 20,
+              backgroundColor: "rgba(17, 242, 21, .8)",
+              padding: 10
+            }}
+          >
+            <Text>Add to Cart</Text>
+          </TouchableOpacity>
+          // <Icon
+          //   style={{ paddingRight: 20 }}
+          //   name={Platform.OS === "ios" ? "ios-cart" : "md-cart"}
+          //   size={20}
+          // ></Icon>
+        )
+      }
+    }
   },
   config
 );
--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -1,9 +1,21 @@
 import React from "react";
-import { View, Button, StyleSheet } from "react-native";
+import { View, Button, StyleSheet, Text } from "react-native";
 
 export default function SignInScreen(props) {
   return (
     <View style={styles.navContainer}>
+      {/* <View style={styles.landingText}> */}
+      <Text style={{ fontSize: 25, fontWeight: "bold" }}>
+        Welcome to GoodUncle!
+      </Text>
+      <Text style={{ fontSize: 15 }}>
+        We believe in food that’s good and good for you. We start by sourcing
+        high quality, local ingredients, then create well-balanced meals that
+        are perfect for everyday eating. Invented in our brains, crafted in our
+        kitchens, transported on our vans, consumed by your mouths. No matter
+        what you’re craving, Good Uncle delivers.
+      </Text>
+      {/* </View> */}
       <Button
         title="Preview Menu"
         onPress={() => props.navigation.navigate("Main")}
@@ -14,8 +26,10 @@ export default function SignInScreen(props) {
 
 const styles = StyleSheet.create({
   navContainer: {
-    alignItems: "center",
-    marginHorizontal: 50,
-    marginTop: 50
+    flex: 1,
+    justifyContent: "space-around",
+    padding: 10,
+    marginHorizontal: 50
+    // justifyItems: "center"
   }
 });
